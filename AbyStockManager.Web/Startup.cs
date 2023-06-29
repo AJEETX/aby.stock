812bed5713667afdc1f56529cbaecfb24ba59fcd
ajeet <ajeetx@email.com>
2023-06-30 00:00:45 +1000

    update to new Db

--- AbyStockManager.Web/Startup.cs
+++ AbyStockManager.Web/Startup.cs
@@ -46,7 +46,7 @@ namespace Aby.StockManager.Web
             //});
 
             services.AddDbContext<EasyStockManagerDbContext>(options =>
-                    options.UseSqlite("Data Source=logo-Jun.db"));
+                    options.UseSqlite("Data Source=sell-Jun.db"));
 
             services.AddAutoMapper(c => c.AddProfile<Aby.StockManager.Mapper.MapProfile>(), typeof(Startup));
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
